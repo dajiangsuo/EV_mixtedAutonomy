7a13351d1e62af83fb2e3f1a79c522f909e63474
Dajiang Suo <djsuo@mit.edu>
2021-08-26 16:15:50 -0400

    adding Jordan controller as the baseline2

--- examples/sumo/inters_Jordan.py
+++ examples/sumo/inters_Jordan.py
@@ -295,125 +295,19 @@ if USE_INFLOWS:
         row_num=N_ROWS,
         additional_net_params=additional_net_params)
 
-flow_params = dict(
-    # name of the experiment
-    exp_tag='traffic_light_grid',
+network = TrafficLightGridNetwork(
+        name="grid-intersection",
+        vehicles=vehicles,
+        net_params=net_params,
+        initial_config=initial_config,
+        traffic_lights=tl_logic)
 
-    # name of the flow environment the experiment is running on
-    #env_name=TrafficLightGridPOEnv,
-    env_name=AccelEnv_forEV,
+env = AccelEnv_forEV(env_params, sim_params, network)
 
-    # name of the network class the experiment is running on
-    network=TrafficLightGridNetwork,
+exp =  Experiment(env)
 
-    # simulator that is used by the experiment
-    simulator='traci',
-
-    # sumo-related parameters (see flow.core.params.SumoParams)
-    sim=SumoParams(
-        sim_step=1,
-        render=True,
-        restart_instance=True,
-    ),
-
-    # environment related parameters (see flow.core.params.EnvParams)
-    env=EnvParams(
-        horizon=HORIZON,
-        #additional_params=additional_env_params,
-        #sims_per_step=5,
-        #warmup_steps=0,
-        additional_params=ADDITIONAL_ENV_PARAMS.copy(),
-    ),
-
-    # network-related parameters (see flow.core.params.NetParams and the
-    # network's documentation or ADDITIONAL_NET_PARAMS component). This is
-    # filled in by the setup_exps method below.
-    net=net_params,
-
-    # vehicles to be placed in the network at the start of a rollout (see
-    # flow.core.params.VehicleParams)
-    veh=vehicles,
-
-    # parameters specifying the positioning of vehicles upon initialization/
-    # reset (see flow.core.params.InitialConfig). This is filled in by the
-    # setup_exps method below.
-    initial=initial_config,
+exp.run(1,1)
 
 
-    # traffic lights to be introduced to specific nodes (see
-    # flow.core.params.TrafficLightParams)
-    tls=tl_logic,
-)
-
-import json
-
-import ray
-try:
-    from ray.rllib.agents.agent import get_agent_class
-except ImportError:
-    from ray.rllib.agents.registry import get_agent_class
-from ray.tune import run_experiments
-from ray.tune.registry import register_env
-
-from flow.utils.registry import make_create_env
-from flow.utils.rllib import FlowParamsEncoder
-
-# number of parallel workers
-N_CPUS = 2
-# number of rollouts per training iteration
-N_ROLLOUTS = 64
-
-ray.shutdown()
-ray.init(num_cpus=N_CPUS+1,object_store_memory=500 * 1024 * 1024)
-
-# The algorithm or model to train. This may refer to "
-#      "the name of a built-on algorithm (e.g. RLLib's DQN "
-#      "or PPO), or a user-defined trainable function or "
-#      "class registered in the tune registry.")
-alg_run = "PPO"
-
-agent_cls = get_agent_class(alg_run)
-config = agent_cls._default_config.copy()
-config["num_workers"] = N_CPUS - 1  # number of parallel workers
-config["train_batch_size"] = HORIZON * N_ROLLOUTS  # batch size
-config["gamma"] = 0.999  # discount rate
-config["model"].update({"fcnet_hiddens": [64, 64]})  # size of hidden layers in network
-config["use_gae"] = True  # using generalized advantage estimation
-config["lambda"] = 0.97  
-config["sgd_minibatch_size"] = min(16 * 1024, config["train_batch_size"])  # stochastic gradient descent
-config["kl_target"] = 0.02  # target KL divergence
-config["num_sgd_iter"] = 10  # number of SGD iterations
-config["horizon"] = HORIZON  # rollout horizon
-
-# save the flow params for replay
-flow_json = json.dumps(flow_params, cls=FlowParamsEncoder, sort_keys=True,
-                       indent=4)  # generating a string version of flow_params
-config['env_config']['flow_params'] = flow_json  # adding the flow_params to config dict
-config['env_config']['run'] = alg_run
-
-# Call the utility function make_create_env to be able to 
-# register the Flow env for this experiment
-create_env, gym_name = make_create_env(params=flow_params, version=0)
-
-# Register as rllib env with Gym
-register_env(gym_name, create_env)
-
-trials = run_experiments({
-    flow_params["exp_tag"]: {
-        "run": alg_run,
-        "env": gym_name,
-        "config": {
-            **config
-        },
-        "checkpoint_freq": 1,  # number of iterations between checkpoints
-        "checkpoint_at_end": True,  # generate a checkpoint at the end
-        "max_failures": 999,
-        "stop": {  # stopping conditions
-            "training_iteration": 1,  # number of iterations to stop after
-        },
-    },
-})
-
-ray.shutdown()
 
 
